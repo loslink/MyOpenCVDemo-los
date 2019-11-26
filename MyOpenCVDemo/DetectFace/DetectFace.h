7c498fd45fe0b193da935c33f73986c43388c624
laoqilian <laoqilian@countrygarden.com.cn>
2019-11-26 21:27:31 +0800

    new

---
+++ MyOpenCVDemo/DetectFace/DetectFace.h
@@ -0,0 +1,12 @@
+#pragma once
+class DetectFace
+{
+
+public:
+	DetectFace();
+	~DetectFace();
+	int detectFace();
+
+};
+
+
---
+++ MyOpenCVDemo/MyOpenCVDemo.cpp
@@ -0,0 +1,20 @@
+﻿// MyOpenCVDemo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "AgeGender/AgeGender.h"
+#include "DetectFace/DetectFace.h"
+
+int main()
+{
+	//年龄检测
+	//ageGender();
+
+	//脸部检测
+	DetectFace detect = DetectFace();
+	detect.detectFace();
+
+	return 0;
+}
+
